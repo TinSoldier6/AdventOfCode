7a8d931395b2a37b0189efb5d74228fac54fc1ea
Jason Gade <jaygade@yahoo.com>
2019-12-06 12:48:28 -0800

    Day 4 part 2 complete

--- 2019/go/Day4/main.go
+++ 2019/go/Day4/main.go
@@ -5,43 +5,76 @@ import (
 	"strconv"
 )
 
-func part1(start, end int) int {
+func part1(start, end int) []int {
 	guesses := []int{}
 
 	for guess := start; guess <= end; guess++ {
-		if isAscending(guess) && hasDouble(guess) {
+		b := []byte(strconv.Itoa(guess))
+		if isAscending(b) && hasDouble(b) {
 			guesses = append(guesses, guess)
 		}
 	}
-	return len(guesses)
+	return guesses
 }
 
-func isAscending(i int) bool {
-	a := []byte(strconv.Itoa(i))
+func part2(list []int) []int {
+	guesses := []int{}
 
-	for n, c := range a {
-		if n+1 < len(a) {
-			if c > a[n+1] {
-				return false
+outer:
+	for _, guess := range list {
+		b := []byte(strconv.Itoa(guess))
+		for start, count := 0, 0; len(b) > 0; b = b[start+count:] {
+			start = findPair(b)
+			count = runLength(b[start:])
+			if count == 2 {
+				guesses = append(guesses, guess)
+				continue outer
 			}
+		}
+	}
+	return guesses
+}
+
+func isAscending(b []byte) bool {
+	for n, c := range b {
+		if n+1 < len(b) && c > b[n+1] {
+			return false
 		}
 	}
 	return true
 }
 
-func hasDouble(i int) bool {
-	a := []byte(strconv.Itoa(i))
+func hasDouble(b []byte) bool {
 
-	for n, c := range a {
-		if n+1 < len(a) {
-			if c == a[n+1] {
-				return true
-			}
+	return findPair(b) < len(b)
+
+}
+
+func findPair(b []byte) int {
+	for n, c := range b {
+		if n+1 < len(b) && c == b[n+1] {
+			return n
 		}
 	}
-	return false
+	return len(b)
+}
+
+func runLength(b []byte) int {
+	if len(b) < 1 {
+		return 0
+	}
+
+	start := b[0]
+	for n, c := range b[1:] {
+		if c != start {
+			return n + 1
+		}
+	}
+	return len(b)
 }
 
 func main() {
-	fmt.Println(part1(387638, 919123))
+	guesses := part1(387638, 919123)
+	fmt.Println(len(guesses))
+	fmt.Println(len(part2(guesses)))
 }
